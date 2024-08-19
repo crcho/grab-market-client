284b6a049796c5e13e8f499e44498231f8ba8874
chaensu <chaensu@gmail.com>
2024-08-19 13:48:06 +0900

    antd 기본 적용

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
+import "antd/dist/antd.css";
 import "./App.css";
 import MainPageComponent from "./main/index.js";
 //import { Switch, Route } from "react-router-dom"; //  Ver 6이상에서는 Switch -> Routes로 변경됨, component -> element로
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom"; // useHistory는 V6에서 useNavigate로 대체
 import UploadPage from "./upload/index.js";
 import ProductPage from "./product/index.js";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 function App() {
+  const navigate = useNavigate(); // const history = useHistory() 대체
+
   return (
     <div>
-      <Routes>
-        <Route exact={true} path="/" element={<MainPageComponent />} />> /
-        <Route exact={true} path="/products/:id" element={<ProductPage />} />
-        <Route exact={true} path="/upload" element={<UploadPage />} />
-      </Routes>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            <img src="/images/icons/logo.png" />
+          </Link>
+          <Button
+            size="large"
+            onClick={function () {
+              navigate("/upload"); //history.push() 대체
+              //alert("업로드 클릭");
+              //console.log("업로드클릭");
+            }}
+            icon={<DownloadOutlined />}
+          >
+            상품업로드
+          </Button>
+        </div>
+      </div>
+
+      <div id="body">
+        <Routes>
+          <Route exact={true} path="/" element={<MainPageComponent />} />> /
+          <Route exact={true} path="/products/:id" element={<ProductPage />} />
+          <Route exact={true} path="/upload" element={<UploadPage />} />
+        </Routes>
+      </div>
+      <div id="footer"></div>
     </div>
   );
 }
--- src/product/index.js
+++ src/product/index.js
@@ -1,9 +1,47 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./index.css";
 
 function ProductPage() {
   const { id } = useParams();
+  const [product, setProduct] = useState(null);
 
-  return <h1> 제품 상세 페이지 {id} 상품 </h1>;
+  useEffect(function () {
+    axios
+      .get(
+        `https://d0d7eb07-bb21-46ab-b5d1-55ddc68b1dc7.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+  console.log(product);
+
+  if (product === null) {
+    return <h1>상품정보를 받고있습니다....</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name"> {product.name} </div>
+        <div id="price"> {product.price}원 </div>
+        <div id="createdAt"> 2020년 7월 8일</div>
+        <div id="description"> {product.description} </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
